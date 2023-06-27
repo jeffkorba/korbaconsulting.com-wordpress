3bd6698b3f4bb3cf54198b71d648320ef92abcb7
Jeff Korba <me@jeffkorba.com>
2023-06-27 20:49:05 +0000

    Fixed Blog Link for Mobile Menu

--- wp-content/themes/korbaconsulting/header.php
+++ wp-content/themes/korbaconsulting/header.php
@@ -142,7 +142,7 @@ global $post;
 
 						<li><a href="/about">About</a></li>
 
-						<li><a href="/about">Blog</a></li>
+						<li><a href="/blog">Blog</a></li>
 
 						<li><a href="/contact">Contact</a></li>
 
